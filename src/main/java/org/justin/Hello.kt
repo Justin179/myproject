b07bff5d8444a2bb7c0adbaec0d5454d1570c1d0
陳君豪 <justin.uob@gmail.com>
2023-09-23 11:41:34 +0800

    initial commit

---
+++ src/main/java/org/justin/Hello.kt
@@ -0,0 +1,5 @@
+package org.justin
+
+fun main() {
+    println("hello")
+}
---
+++ src/main/java/org/justin/Main2.java
@@ -0,0 +1,7 @@
+package org.justin;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("test again");
+    }
+}
